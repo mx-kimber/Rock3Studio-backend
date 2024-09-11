28804410dbc4a0e8f10e5adbbc0623ad0707f06a
Kimber Ogden <mx.kimber@gmail.com>
2024-09-10 22:35:25 -0500

    Refactor User model validations to handle password conditionally

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,16 @@ class User < ApplicationRecord
   validates :full_name, presence: true
   validates :user_name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true
 
+  validates :password, presence: true, confirmation: true, on: :create
+
+  validate :password_validation_on_update, if: :password
+
+  private
+
+  def password_validation_on_update
+    if password.present? && password_confirmation.blank?
+      errors.add(:password_confirmation, "can't be blank")
+    end
+  end
 end
